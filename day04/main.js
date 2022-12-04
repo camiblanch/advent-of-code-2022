f5367d94ed786eeeb0db3aab9d7eef053e869dc8
Cami Blanch <caminblanch@gmail.com>
2022-12-03 22:24:21 -0700

    Day 4 part 1

--- day04/main.js
+++ day04/main.js
@@ -4,6 +4,32 @@ const main = () => {
 	const fileName = "./input.txt";
 	const rawData = readFile(fileName);
 
+	let pairsWithFullyContains = 0;
+
+	rawData.forEach(pair => {
+		const [elf1, elf2] = pair.split(",");
+		const [elf1Start, elf1End] = elf1.split("-");
+		const [elf2Start, elf2End] = elf2.split("-");
+
+		const elf1Range = createRange(elf1Start, elf1End);
+		const elf2Range = createRange(elf2Start, elf2End);
+
+		if (elf1Range.includes(elf2Range) || elf2Range.includes(elf1Range)) {
+			pairsWithFullyContains++;
+		}
+	});
+
+	console.log("Fully contains: ", pairsWithFullyContains);
+};
+
+const createRange = (start, end) => {
+	const startInt = parseInt(start);
+	const endInt = parseInt(end);
+	let range = "-";
+	for (let i = startInt; i <= endInt; i++) {
+		range += i + "-";
+	}
+	return range;
 };
 
 main();
