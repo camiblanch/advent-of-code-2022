dff173c7fbca8d11125767484494ee491de3ec39
Cami Blanch <caminblanch@gmail.com>
2022-12-06 23:26:33 -0700

    Day 7 part 2

---
+++ day07/part2.js
@@ -0,0 +1,91 @@
+const {readFile} = require("../utils.js");
+
+class Directory {
+	constructor(dirName, parentDir) {
+		this.dirName = dirName;
+		this.subDirs = [];
+		this.files = [];
+		this.parentDir = parentDir;
+		this.dirSize = 0;
+	}
+
+	addFile = (file) => {
+		this.files.push(file);
+		this.addToDirSize(file.size);
+	};
+
+	addToDirSize = (moreSize) => {
+		this.dirSize += moreSize;
+		if (this.parentDir) {
+			this.parentDir.addToDirSize(moreSize);
+		}
+	};
+}
+
+class File {
+	constructor(size, name) {
+		this.size = size;
+		this.name = name;
+	}
+}
+
+const startsWithNumber = (s) => {
+	return /^\d/.test(s);
+};
+
+const part2 = () => {
+	const fileName = "./input.txt";
+	const rawData = readFile(fileName);
+
+	const fileSystem = new Directory("/");
+	let currDir = fileSystem;
+	// skip first 2 lines since we already know where we are starting
+	for (let i = 2; i < rawData.length; i++) {
+		const line = rawData[i];
+		if (line.startsWith("dir ")) {
+			const dirName = line.substring(4);
+			currDir.subDirs.push(new Directory(dirName, currDir));
+		} else if (startsWithNumber(line)) {
+			const [fileSize, fileName] = line.split(" ");
+			currDir.addFile(new File(parseInt(fileSize), fileName));
+		} else if (line.startsWith("$ cd")) {
+			const dirToGoTo = line.substring(5);
+			if (dirToGoTo === "..") {
+				currDir = currDir.parentDir;
+			} else if (dirToGoTo === "/") {
+				currDir = fileSystem;
+			} else {
+				currDir = currDir.subDirs.find(subDir => subDir.dirName === dirToGoTo);
+			}
+		} else if (line.startsWith("$ ls")) {
+			// do nothing?
+		}
+	}
+
+	const fileSystemDiskSpace = 70000000;
+	const updateSpaceNeeded = 30000000;
+
+	const unusedSpace = fileSystemDiskSpace - fileSystem.dirSize;
+	const minNeededToDelete = updateSpaceNeeded - unusedSpace;
+
+	let subDirToDelete = fileSystem;
+
+	const findDirToDelete = (dir) => {
+		dir.subDirs.forEach(subDir => {
+			if (subDir.dirSize >= minNeededToDelete) {
+				if (subDir.dirSize < subDirToDelete.dirSize) {
+					subDirToDelete = subDir;
+				}
+				findDirToDelete(subDir);
+			}
+		});
+	};
+
+	findDirToDelete(fileSystem);
+	console.log("Delete dir: ", subDirToDelete.dirName);
+	console.log("to delete: ", subDirToDelete.dirSize);
+};
+
+part2();
+3956976;
+4266905;
